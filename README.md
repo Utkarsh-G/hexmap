70b5841c66749bef7642fc522a130c2b4dc0deab
Utkarsh <utkarsh@Utkarshs-MacBook-Pro.local>
2023-07-21 18:28:00 -0400

    a big hexmap in gradient

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 https://slowe.github.io/stuquery/hexmap.html
 
+Hexmap conversion (from csv to hexjson) only works on google chrome
 Hexmap builder https://open-innovations.org/projects/hexmaps/builder.html
--- mapgen.js
+++ mapgen.js
@@ -1,7 +1,7 @@
 let mystr = ""
-for (let i = 0; i < 20; i++) {
-  for (let j = 0; j < 43; j++){
-    let id = 1000+43*i+j;
+for (let i = 0; i < 43; i++) {
+  for (let j = 0; j < 20; j++){
+    let id = 1000+20*i+j;
         mystr = mystr + id + `,"Unnamed",${i},${j},"Unexplored","None","rgb(${5*i}, ${3*j}, 102)",""\n`
   }
 }
